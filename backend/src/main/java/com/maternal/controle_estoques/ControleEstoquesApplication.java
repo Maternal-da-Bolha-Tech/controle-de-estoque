3ef92a4600d2271974ed780a45bae0fd2968efa4
Danilo Martins Gazzoli <dmartinsgazzoli@gmail.com>
2025-03-06 22:07:21 -0300

    chore(backend): criação da base spring
    .

---
+++ backend/src/main/java/com/maternal/controle_estoques/ControleEstoquesApplication.java
@@ -0,0 +1,13 @@
+package com.maternal.controle_estoques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoquesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/maternal/controle_estoques/ControleEstoquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maternal.controle_estoques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
