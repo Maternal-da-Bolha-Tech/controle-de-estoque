ba6d10cd395559c02ff07ad73379591dcdff00fd
zilohnoji <donatoordep@gmail.com>
2025-03-19 12:24:07 -0300

    created static mappers

--- backend/src/main/java/com/maternal/controle_estoques/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/com/maternal/controle_estoques/service/impl/ProductServiceImpl.java
@@ -1,7 +1,7 @@
 package com.maternal.controle_estoques.service.impl;
 
 import com.maternal.controle_estoques.dto.ProductDTO;
-import com.maternal.controle_estoques.mapper.IProductMapper;
+import com.maternal.controle_estoques.mapper.model.ProductMapper;
 import com.maternal.controle_estoques.model.Product;
 import com.maternal.controle_estoques.repository.IProductRepository;
 import com.maternal.controle_estoques.service.IProductService;
@@ -10,16 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductServiceImpl implements IProductService {
   private final IProductRepository productRepository;
-  private final IProductMapper productMapper;
 
-  public ProductServiceImpl(IProductRepository productRepository, IProductMapper productMapper) {
+  public ProductServiceImpl(IProductRepository productRepository) {
     this.productRepository = productRepository;
-    this.productMapper = productMapper;
   }
 
   @Override
-  public ProductDTO insertNewProduct(ProductDTO newProduct) {
-    Product productSaved = this.productRepository.save(this.productMapper.DtoToEntity(newProduct));
-    return this.productMapper.entityToDto(productSaved);
+  public ProductDTO insertNewProduct(ProductDTO requestDto) {
+    Product productSaved = this.productRepository.save(ProductMapper.dtoToEntity(requestDto));
+    return ProductMapper.entityToDto(productSaved);
   }
 }
