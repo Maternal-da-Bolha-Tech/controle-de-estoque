e12f0e745462528c6c136425ca18c4c47622e94d
zilohnoji <donatoordep@gmail.com>
2025-03-19 11:39:56 -0300

    alter uris from the controllers

--- backend/src/main/java/com/maternal/controle_estoques/controller/ProductController.java
+++ backend/src/main/java/com/maternal/controle_estoques/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.maternal.controle_estoques.dto.ProductDTO;
 import com.maternal.controle_estoques.service.IProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/product")
 public class ProductController {
-  private final IProductService productService;
+	private final IProductService productService;
 
-  public ProductController(IProductService productService) {
-    this.productService = productService;
-  }
+	public ProductController(IProductService productService) {
+		this.productService = productService;
+	}
 
-  @PostMapping(value = "/insert", consumes = "application/json", produces = "application/json")
-  public ResponseEntity<ProductDTO> insertNewProduct(@RequestBody @Valid ProductDTO newProduct) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(this.productService.insertNewProduct(newProduct));
-  }
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ProductDTO> insertNewProduct(@RequestBody @Valid ProductDTO newProduct) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.productService.insertNewProduct(newProduct));
+	}
 }
