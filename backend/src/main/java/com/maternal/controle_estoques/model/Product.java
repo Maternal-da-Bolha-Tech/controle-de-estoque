3969049d5c5388a2b25c0820d97c38e1e540d616
zilohnoji <donatoordep@gmail.com>
2025-03-19 12:08:39 -0300

    private getters/setters and constructor

--- backend/src/main/java/com/maternal/controle_estoques/model/Product.java
+++ backend/src/main/java/com/maternal/controle_estoques/model/Product.java
@@ -11,81 +11,81 @@ import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 
-@Entity(name = "product")
+@Entity
 @Table(name = "tb_product")
 public class Product {
 
-  @Id
-  @NotBlank(message = "The name of the product should not be null and void.")
-  @Column(name = "product_name")
-  private String productName;
+	@Id
+	@NotBlank(message = "The name of the product should not be null and void.")
+	@Column(name = "product_name")
+	private String productName;
 
-  @NotBlank(message = "The description of the product should not be null and void.")
-  @Column(name = "product_description", nullable = false)
-  private String productDescription;
+	@NotBlank(message = "The description of the product should not be null and void.")
+	@Column(name = "product_description", nullable = false)
+	private String productDescription;
 
-  @NotNull(message = "The price of the product should not be null.")
-  @Positive(message = "The price of the product must be positive greater than zero.")
-  @Column(name = "product_price", nullable = false, precision = 10, scale = 2)
-  private BigDecimal productPrice;
+	@NotNull(message = "The price of the product should not be null.")
+	@Positive(message = "The price of the product must be positive greater than zero.")
+	@Column(name = "product_price", nullable = false, precision = 10, scale = 2)
+	private BigDecimal productPrice;
 
-  @NotNull(message = "The quantity of the product should not be null.")
-  @PositiveOrZero(message = "The quantity of the product must be greater than or equal to zero.")
-  @Column(name = "product_amount", nullable = false)
-  private Integer productAmount;
+	@NotNull(message = "The quantity of the product should not be null.")
+	@PositiveOrZero(message = "The quantity of the product must be greater than or equal to zero.")
+	@Column(name = "product_amount", nullable = false)
+	private Integer productAmount;
 
-  public Product() {
-  }
+	private Product() {
+	}
 
-  public Product(String productName, String productDescription, BigDecimal productPrice, Integer productAmount) {
-    this.productName = productName;
-    this.productDescription = productDescription;
-    this.productPrice = productPrice;
-    this.productAmount = productAmount;
-  }
+	public Product(String productName, String productDescription, BigDecimal productPrice, Integer productAmount) {
+		this.productName = productName;
+		this.productDescription = productDescription;
+		this.productPrice = productPrice;
+		this.productAmount = productAmount;
+	}
 
-  public String getProductName() {
-    return productName;
-  }
+	public String getProductName() {
+		return productName;
+	}
 
-  public void setProductName(String productName) {
-    this.productName = productName;
-  }
+	public BigDecimal getProductPrice() {
+		return productPrice;
+	}
 
-  public String getProductDescription() {
-    return productDescription;
-  }
+	public String getProductDescription() {
+		return productDescription;
+	}
 
-  public void setProductDescription(String productDescription) {
-    this.productDescription = productDescription;
-  }
+	private void setProductName(String productName) {
+		this.productName = productName;
+	}
 
-  public BigDecimal getProductPrice() {
-    return productPrice;
-  }
+	public Integer getProductAmount() {
+		return productAmount;
+	}
 
-  public void setProductPrice(BigDecimal productPrice) {
-    this.productPrice = productPrice;
-  }
+	private void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
 
-  public Integer getProductAmount() {
-    return productAmount;
-  }
+	private void setProductPrice(BigDecimal productPrice) {
+		this.productPrice = productPrice;
+	}
 
-  public void setProductAmount(Integer productAmount) {
-    this.productAmount = productAmount;
-  }
+	private void setProductAmount(Integer productAmount) {
+		this.productAmount = productAmount;
+	}
 
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
 
-    Product product = (Product) o;
-    return productName.equals(product.productName);
-  }
+		Product product = (Product) o;
+		return productName.equals(product.productName);
+	}
 
-  @Override
-  public int hashCode() {
-    return productName.hashCode();
-  }
+	@Override
+	public int hashCode() {
+		return productName.hashCode();
+	}
 }
